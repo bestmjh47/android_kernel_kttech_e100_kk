861ba228d9a5dcc8c45793e70232c6fc5d44d11f
moon <bestmjh47@naver.com>
2014-02-09 16:07:25 +0900

    Revert "Bump to Linux-3.4.5"
    This reverts commit db40c93371c28f236e7f474f59d9afa2da22cbfc.
    
    Conflicts:
    	.version
    	Module.symvers
    	include/config/kernel.release
    	include/generated/compile.h
    	include/generated/utsrelease.h
    	zImage
    
    cccccccccccccccccc

--- include/generated/utsrelease.h
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-#define UTS_RELEASE "3.4.5-Ultimate_KERNEL-E100-gdb40c93-dirty"
-=======
-#define UTS_RELEASE "3.4.1-Ultimate_KERNEL-E100-g92a8e7b-dirty"
->>>>>>> parent of db40c93... Bump to Linux-3.4.5
