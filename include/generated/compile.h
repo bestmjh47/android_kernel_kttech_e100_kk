ae7437c560c04b7f0529b6f31fb5d7e46bee8793
moon <bestmjh47@naver.com>
2014-02-20 13:03:08 +0900

    add intelli-plug

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Thu Feb 20 12:50:12 KST 2014"
+#define UTS_VERSION "#3 SMP PREEMPT Thu Feb 20 13:01:33 KST 2014"
 #define LINUX_COMPILE_BY "moon"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.8.3 20131202 (prerelease) (crosstool-NG hg+default-4cfbdb295328 - Linaro GCC 2013.12 - Cortex-A8) "
