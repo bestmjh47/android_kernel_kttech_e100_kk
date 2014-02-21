cb981bcd8f1e40c4bf7aecc14f21ee961dd9b05f
moon <bestmjh47@naver.com>
2014-02-21 12:26:28 +0900

    Add fiops

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 30 */
+/* This file is auto generated, version 32 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#30 SMP PREEMPT Fri Feb 21 12:14:33 KST 2014"
+#define UTS_VERSION "#32 SMP PREEMPT Fri Feb 21 12:25:46 KST 2014"
 #define LINUX_COMPILE_BY "moon"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.8.3 20131202 (prerelease) (crosstool-NG hg+default-4cfbdb295328 - Linaro GCC 2013.12 - Cortex-A8) "
