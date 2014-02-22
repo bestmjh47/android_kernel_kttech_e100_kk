ea8206109c8642d56520e6efc3232f8523fd5aa5
moon <bestmjh47@naver.com>
2014-02-22 18:07:30 +0900

    Clean up!

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 37 */
+/* This file is auto generated, version 40 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#37 SMP PREEMPT Fri Feb 21 16:23:11 KST 2014"
+#define UTS_VERSION "#40 SMP PREEMPT Sat Feb 22 18:06:41 KST 2014"
 #define LINUX_COMPILE_BY "moon"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.8.3 20140106 (prerelease) (crosstool-NG linaro-1.13.1+bzr2644 - Linaro GCC 2014.01) "
