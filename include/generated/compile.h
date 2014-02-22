d66330d80c5b197228fb3f7698e7a20e03d96d5c
moon <bestmjh47@naver.com>
2014-02-22 20:39:40 +0900

    Use Linaro GCC 4.8.3 optimized for Cortex-A9

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 44 */
+/* This file is auto generated, version 45 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#44 SMP PREEMPT Sat Feb 22 18:57:34 KST 2014"
+#define UTS_VERSION "#45 SMP PREEMPT Sat Feb 22 20:31:59 KST 2014"
 #define LINUX_COMPILE_BY "moon"
 #define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILER "gcc version 4.8.3 20131202 (prerelease) (crosstool-NG hg+default-4cfbdb295328 - Linaro GCC 2013.12 - Cortex-A8) "
+#define LINUX_COMPILER "gcc version 4.8.3 20140106 (prerelease) (crosstool-NG linaro-1.13.1+bzr2644 - Linaro GCC 2014.01) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.4-Ultimate_KERNEL-E100-ge830774-dirty"
+#define UTS_RELEASE "3.4.4-Ultimate_KERNEL-E100-ga4b67b2-dirty"
