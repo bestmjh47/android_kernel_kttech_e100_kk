0aaff30dbad8f659e80d50569ec4eb211a9c8e1c
moon <bestmjh47@naver.com>
2014-04-05 16:36:46 +0900

    Add nightmare, smartassH3

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 56 */
+/* This file is auto generated, version 63 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#56 SMP PREEMPT Sat Apr 5 15:32:58 KST 2014"
+#define UTS_VERSION "#63 SMP PREEMPT Sat Apr 5 16:32:39 KST 2014"
 #define LINUX_COMPILE_BY "moon"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.8.3 20140106 (prerelease) (crosstool-NG linaro-1.13.1+bzr2644 - Linaro GCC 2014.01) "
