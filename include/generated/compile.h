d330de9099fff8d9fa55db93c918cd25c65769d0
moon <bestmjh47@naver.com>
2014-04-06 18:30:26 +0900

    Enable Zram, Zcache

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 64 */
+/* This file is auto generated, version 65 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#64 SMP PREEMPT Sat Apr 5 17:18:31 KST 2014"
+#define UTS_VERSION "#65 SMP PREEMPT Sun Apr 6 18:29:14 KST 2014"
 #define LINUX_COMPILE_BY "moon"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.8.3 20140106 (prerelease) (crosstool-NG linaro-1.13.1+bzr2644 - Linaro GCC 2014.01) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.4-Ultimate_KERNEL-E100-ge5730b9-dirty"
+#define UTS_RELEASE "3.4.4-Ultimate_KERNEL-E100-g1aa075b-dirty"
